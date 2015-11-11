ce6b21a1a421a1b53b7a02b4f6e3f6ffc0f45ed9
pfordahl <pfordahl78@gmail.com>
2015-11-11 14:29:32 -0600

    Fixed spelling

--- device-gateway/files/default/env-configure.sh
+++ device-gateway/files/default/env-configure.sh
@@ -1,10 +1,7 @@
-#!bin/bash
+#!/bin/bash
 logfile=/var/log/env-configure.log
 #Start the logging
 exec >> $logfile 2>&1
 
 #Create the configure directory
 mkdir -p /opt/bamHome/config/bam/
-
-#Create Environment directory
-mkdir -p /etc/environment
